b175128e3904b2bc3e79164237bb84a36a8e06f9
q10242 <q10242@gmail.com>
2021-04-15 13:59:01 +0800

    add migration files

---
+++ database/migrations/2021_04_15_055119_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('comments')->nullable()->comment('評論');
+            $table->integer('post_id')->nullable()->comment('文章id');
+            $table->foreign('post_id')->references('id')->on('post');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
